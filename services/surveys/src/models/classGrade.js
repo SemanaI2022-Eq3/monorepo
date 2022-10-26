efdfc417ebf579df045a07f92e2557fc32816e10
Luis F. Lomelín Ibarra <luis.f.lomelin.il@gmail.com>
2022-10-26 12:15:48 -0500

    Mongoose Schemas are ready

---
+++ services/surveys/src/models/classGrade.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const classGradeSchema = new mongoose.Schema(
+    {
+        
+        class:{
+            type: mongoose.ObjectId,
+            ref:'Class',
+            required: false
+        },
+        clarity :{
+            type: Number,
+            required: true
+        },
+        helpOffered : {
+            type: Number,
+            required: true
+        },
+        classDifficulty: {
+            type: Number,
+            required: true
+        },
+        formTags : {
+            type: Array,
+            required : true
+        },
+        comments : {
+            type: String,
+            required : true
+        },
+        doRecommend :{
+            type : Boolean,
+            require: true 
+        },
+        obligatoryLectureAssitance : {
+            type : Boolean,
+            required : true
+        },
+        finalGrade:{
+            type : Number,
+            required: false
+        }
+
+    }
+);
+
+module.exports = mongoose.model('Teacher', classGradeSchema)
