8d52424d28e6c191c9c26bbcdd400afd4a1738ed
Rodrigo Casale <popobasura3@hotmail.com>
2022-10-26 11:07:41 -0500

    routing nitfix

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,15 +3,20 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./views/Home";
 import Layout from "./components/Layout";
 import { AuthContextProvider } from "./context/auth-context";
+import { CssVarsProvider } from "@mui/joy/styles";
 
 const App = () => (
   <>
     <AuthContextProvider>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Home />} />
-        </Route>
-      </Routes>
+      <CssVarsProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/teachers" element={<Home />} />
+            <Route path="/universities" element={<Home />} />
+          </Route>
+        </Routes>
+      </CssVarsProvider>
     </AuthContextProvider>
   </>
 );
