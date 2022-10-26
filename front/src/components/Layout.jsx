9e0034c87370cb82fc85fd85f5de67e2defe8d30
Rodrigo Casale <popobasura3@hotmail.com>
2022-10-26 11:08:19 -0500

    header added

--- front/src/components/Layout.jsx
+++ front/src/components/Layout.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+import Header from "./Header";
 
 export default function Layout() {
   return (
     <>
       <main>
+        <Header />
         <Outlet />
       </main>
     </>
