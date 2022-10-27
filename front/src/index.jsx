fc117e53ec476f1b1f35369caaf7a3d50fae02ea
PatoGuereque <contacto@patoguereque.com>
2022-10-26 23:05:44 -0500

    fix: Add withCredentials as axios defaults

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { CssVarsProvider } from "@mui/joy/styles";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
