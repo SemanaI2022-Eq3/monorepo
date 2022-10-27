63c1636d45dfd7225ceded66d5de02c733873d3d
Luis F. Lomelín Ibarra <luis.f.lomelin.il@gmail.com>
2022-10-27 17:48:04 -0500

    Minor fixes

--- services/surveys/src/routes/teacher.js
+++ services/surveys/src/routes/teacher.js
@@ -8,39 +8,44 @@ import dbconn from '../models/dbconn';
 
 const teachersR = express();
 
-teachersR.post('/teacher', authRequired, async (_req, res, next) => {
-  try {
-    // const teachersToAdd = ['Luis', 'María', 'Sara', 'Pedro'];
-    // const teachDpto = ['Mate', 'Español', 'Historia', 'Quimica'];
-    const teachesClass = [
-      'Mate 1',
-      'Quimica Organica',
-      'Historia y el ambiente Global',
-      'Análisis y Expresión Verbal',
-    ];
-    const x = Math.round(Math.random() * teachesClass); // Asigna una materia al azar
-    const selectedClass = teachesClass[x];
+teachersR.post(
+  '/teacher',
+  authRequired,
+  authRequired,
+  async (_req, res, next) => {
+    try {
+      // const teachersToAdd = ['Luis', 'María', 'Sara', 'Pedro'];
+      // const teachDpto = ['Mate', 'Español', 'Historia', 'Quimica'];
+      const teachesClass = [
+        'Mate 1',
+        'Quimica Organica',
+        'Historia y el ambiente Global',
+        'Análisis y Expresión Verbal',
+      ];
+      const x = Math.round(Math.random() * teachesClass); // Asigna una materia al azar
+      const selectedClass = teachesClass[x];
 
-    const tname = `${_req.body.firstName} ${_req.body.lastName}`;
-    const teacher = new Teacher({
-      name: tname,
-      department: selectedClass,
-      institution: _req.body.School,
-    });
-    // eslint-disable-next-line no-await-in-loop
-    const teach = await teacher.save();
-    // eslint-disable-next-line no-underscore-dangle
-    const classes = new Class({ name: selectedClass, teacher: teach._id });
-    // eslint-disable-next-line no-await-in-loop
-    await classes.save();
+      const tname = `${_req.body.firstName} ${_req.body.lastName}`;
+      const teacher = new Teacher({
+        name: tname,
+        department: selectedClass,
+        institution: _req.body.School,
+      });
+      // eslint-disable-next-line no-await-in-loop
+      const teach = await teacher.save();
+      // eslint-disable-next-line no-underscore-dangle
+      const classes = new Class({ name: selectedClass, teacher: teach._id });
+      // eslint-disable-next-line no-await-in-loop
+      await classes.save();
 
-    res.status(200).json({
-      msg: 'ok',
-    });
-  } catch (error) {
-    next(error);
+      res.status(200).json({
+        msg: 'ok',
+      });
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
 teachersR.get('/teacher/:teacherId', authRequired, async (_req, res, next) => {
   try {
@@ -50,7 +55,7 @@ teachersR.get('/teacher/:teacherId', authRequired, async (_req, res, next) => {
       const classes = await Class.find({ teacher: teachers._id }).populate(
         'teacher'
       );
-      console.log(classes);
+
       let grades = [];
       if (classes != null) {
         for (let i = 0; i < classes.length; i += 1) {
@@ -74,7 +79,7 @@ teachersR.get('/teacher/:teacherId', authRequired, async (_req, res, next) => {
   }
 });
 
-teachersR.get('/teachers', authRequired, async (_req, res, next) => {
+teachersR.get('/teachers', async (_req, res, next) => {
   try {
     const teachers = await Teacher.find({});
 
