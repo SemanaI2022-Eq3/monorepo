6946b4486353d4cf124b9d11f15d873bf980bdc5
PatoGuereque <contacto@patoguereque.com>
2022-10-28 12:14:57 -0500

    fix: Fix School hook not using state

--- front/src/views/School.jsx
+++ front/src/views/School.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Box from "@mui/joy/Box";
 import Sheet from "@mui/joy/Sheet";
 import Grid from "@mui/joy/Grid";
@@ -12,18 +12,6 @@ function randomNumberInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let profesors = [
-  "Citlali Ritter",
-  "Javonte Grover",
-  "Josh Dominguez",
-  "Javion Pullen",
-  "Leigh Durant",
-  "Suzanne Blount",
-  "Christian Wagner",
-  "Iris Rubio",
-  "Dante Slone",
-  "Maddie Hatfield",
-];
 const schoolCals = [
   "Reputacion",
   "Oportunidades",
@@ -32,46 +20,15 @@ const schoolCals = [
   "Ubicacion",
 ];
 
-const listSchoolCal = schoolCals.map((schoolCal) => (
-  <Box m={5} sx={{ display: "flex" }}>
-    <Typography px={2}>
-      {" "}
-      {schoolCal}
-      <Typography> {randomNumberInRange(60, 100)} </Typography>
-    </Typography>
-    <StarIcon fontSize="large"></StarIcon>
-  </Box>
-));
-
-let listProfesors = profesors.map((profesor, key) => (
-  <Box m={5} sx={{ display: "flex" }}>
-    <AccountCircleIcon />
-    <Box>
-      <Typography px={12}> {profesor} </Typography>
-      <Typography px={12}>Reseñas {randomNumberInRange(60, 100)}</Typography>
-    </Box>
-  </Box>
-));
-
 export default function School() {
-  axios.get(`${SURVEY_ENDPOINT}/api/survey/teachers`).then((res) => {
-    profesors = [];
-    const teachers = res.data.teachers;
-    for (let i = 0; i < teachers.length; i += 1) {
-      profesors.push(teachers[i].name);
-    }
-    listProfesors = profesors.map((profesor, key) => (
-      <Box m={5} sx={{ display: "flex" }}>
-        <AccountCircleIcon />
-        <Box>
-          <Typography px={12}> {profesor} </Typography>
-          <Typography px={12}>
-            Reseñas {randomNumberInRange(60, 100)}
-          </Typography>
-        </Box>
-      </Box>
-    ));
-  });
+  const [teachers, setTeachers] = useState([]);
+
+  React.useEffect(() => {
+    axios
+      .get(`${SURVEY_ENDPOINT}/api/survey/teachers`)
+      .then((res) => setTeachers(res.data.teachers));
+  }, []);
+
   return (
     <Box sx={{ flexGrow: 1, width: "98%" }}>
       <Grid container spacing={2}>
@@ -91,7 +48,17 @@ export default function School() {
               <Typography variant="h5" m={5}>
                 Profesores mejores calificados
               </Typography>
-              {listProfesors}
+              {teachers.map(({ name: teacherName }) => (
+                <Box m={5} sx={{ display: "flex" }} key={teacherName}>
+                  <AccountCircleIcon />
+                  <Box>
+                    <Typography px={12}> {teacherName} </Typography>
+                    <Typography px={12}>
+                      Reseñas {randomNumberInRange(60, 100)}
+                    </Typography>
+                  </Box>
+                </Box>
+              ))}
             </Sheet>
           </Box>
         </Grid>
@@ -111,7 +78,16 @@ export default function School() {
               <Typography variant="h5" m={5}>
                 Calificacion de la escuela {}
               </Typography>
-              {listSchoolCal}
+              {schoolCals.map((schoolCal) => (
+                <Box m={5} sx={{ display: "flex" }} key={schoolCal}>
+                  <Typography px={2}>
+                    {" "}
+                    {schoolCal}
+                    <Typography> {randomNumberInRange(60, 100)} </Typography>
+                  </Typography>
+                  <StarIcon fontSize="large"></StarIcon>
+                </Box>
+              ))}
             </Sheet>
           </Box>
         </Grid>
